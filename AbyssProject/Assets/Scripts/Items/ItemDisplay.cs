d552d51188de8983afb6ffbbea47d9485c76adb1
OlivierArgentieri <olivierargentieri@gmail.com>
2019-08-07 16:09:05 +0200

    start key binding menu 0.5/2

---
+++ AbyssProject/Assets/Scripts/Items/ItemDisplay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ItemDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Item m_item;
+    void OnValidate()
+    {
+        Debug.Log(m_item.m_name); 
+        Debug.Log(m_item.m_description);
+    }
+}
